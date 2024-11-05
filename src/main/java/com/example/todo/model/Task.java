b0ede9af77a26c2c2d9d83b1640c45f5fc44b17e
Williane <willy.pinheiro.94@gmail.com>
2024-11-05 17:40:03 -0300

    add arquivos iniciais

---
+++ src/main/java/com/example/todo/model/Task.java
@@ -0,0 +1,27 @@
+package com.example.todo.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private boolean completed = false;
+
+}
